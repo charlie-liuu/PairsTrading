5e80b2922b4205fa3df43e7f0940cde96e6c33a6
Charlie Liu <c596liu@uwaterloo.ca>
2022-12-28 22:08:33 -0500

    Added New Pairs Trading Main File

---
+++ pairs.py
@@ -0,0 +1,6 @@
+from statistics import mean
+from statsmodels.regression.linear_model import OLS
+from statsmodels.tsa.stattools import coint
+import matplotlib.pyplot as plt
+from hurst import compute_Hc
+import math
