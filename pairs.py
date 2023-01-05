56dac6a4d02b45850164b271d4ea5a1da14e83b3
Charlie_liuu <c596liu@uwaterloo.ca>
2023-01-05 17:20:26 -0500

    Testing

--- pairs.py
+++ pairs.py
@@ -3,4 +3,6 @@ from statsmodels.regression.linear_model import OLS
 from statsmodels.tsa.stattools import coint
 import matplotlib.pyplot as plt
 from hurst import compute_Hc
-import math
+import mat
+
+for 
