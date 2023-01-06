e638f55b9054f4693559f6af79013615733cc210
Charlie_liuu <c596liu@uwaterloo.ca>
2023-01-05 23:25:10 -0500

    Deleted Original algorithm python file and replaced with jupyter notebook

--- pairs.py
+++
@@ -1,8 +0,0 @@
-from statistics import mean
-from statsmodels.regression.linear_model import OLS
-from statsmodels.tsa.stattools import coint
-import matplotlib.pyplot as plt
-from hurst import compute_Hc
-import mat
-
-for 
